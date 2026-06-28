8fd050b731aeff060f4f67b4e2d6ba8a12fad38a
seoyeon866-star <seoyeon866@gmail.com>
2026-06-29 00:23:08 +0900

    페이지 이동 시 scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import SearchResult from './pages/SearchResult';
 import ProductDetail from './pages/ProductDetail';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname]);
+  return null;
+}
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/search" element={<SearchResult />} />
-      <Route path="/product/:id" element={<ProductDetail />} />
-      <Route path="/recall/:id" element={<ProductDetail />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<SearchResult />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/recall/:id" element={<ProductDetail />} />
+      </Routes>
+    </>
   );
 }
